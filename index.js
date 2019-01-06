6cc38dd17d432242b67fdd13e9eb5416eef3600f
christieg <minifighterjets@gmail.com>
2019-01-06 17:55:39 -0600

    added jquery?

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var shiftWindow = function () { scrollBy(0, -60)};
 window.addEventListener("hashchange", shiftWindow);
 function load() { if (window.location.hash) shiftWindow();}
+$('body').scrollspy({ target: '#navbar' })
