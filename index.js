16b46a8da44bc3c7d1f4e82defab00840d260d3b
cjf2222 <cjf2222@tamu.edu>
2019-01-08 11:08:36 -0600

    move things
    moved examples to ex and tried to get offset scrolling to work.

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-//var shiftWindow = function () { scrollBy(0, -60)};
-//window.addEventListener("hashchange", shiftWindow);
-//function load() { if (window.location.hash) shiftWindow();}
-// fix sub nav on scroll
+
 /*
-var $win = $(window)
-  , $nav = $('.subnav')
-  , navTop = $('.subnav').length && $('.subnav').offset().top - 40
-  , isFixed = 0
-
-processScroll()
-
-// hack sad times - holdover until rewrite for 2.1
-$nav.on('click', function () {
-  if (!isFixed) setTimeout(function () {  $win.scrollTop($win.scrollTop() - 47) }, 10)
-})
-
-$win.on('scroll', processScroll)
-
-function processScroll() {
-  var i, scrollTop = $win.scrollTop()
-  if (scrollTop >= navTop && !isFixed) {
-    isFixed = 1
-    $nav.addClass('subnav-fixed')
-  } else if (scrollTop <= navTop && isFixed) {
-    isFixed = 0
-    $nav.removeClass('subnav-fixed')
-  }
-}
+var shiftWindow = function () { scrollBy(0, -60)};
+window.addEventListener("hashchange", shiftWindow);
+function load() { if (window.location.hash) shiftWindow();}
 */
+/*
+$('.main-content').scrollspy({
+    target: '#navbar',
+    offset: 0
+  });
+  */
+
+document.write("WHAT UPUPUPUPUPUPUP");
