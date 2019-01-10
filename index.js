e2797de9bb314440dd41b35932c9b9d73444865a
christieg <minifighterjets@gmail.com>
2019-01-09 22:05:23 -0600

    parallaxing, fonts, hamburger icon, collapsing navbar

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 
-/*
-var shiftWindow = function () { scrollBy(0, -60)};
+var offset = $('#navbar').height() + 20;
+console.log("scrolled by " + offset);
+var shiftWindow = function () { scrollBy(0, -offset)
+                              console.log("scrolled by " + offset);};
 window.addEventListener("hashchange", shiftWindow);
 function load() { if (window.location.hash) shiftWindow();}
-*/
-/*
-$('.main-content').scrollspy({
-    target: '#navbar',
-    offset: 0
-  });
-  */
 
-document.write("WHAT UPUPUPUPUPUPUP");
+console.log(offset);
+$('body').scrollspy({
+    target: '#navbar',
+    offset: offset
+  });
+  
+// this code belongs to Jesse Couch on Code Pen https://codepen.io/designcouch/pen/Atyop
+$(document).ready(function(){
+	$('#nav-icon2').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
