e74e8a07b3517213baa5c7ca29c9d7236dde2032
christieg <minifighterjets@gmail.com>
2019-01-15 16:35:43 -0600

    smoother scrolling

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 
 var offset = $('#navbar').height() + 20;
-console.log("scrolled by " + offset);
-var shiftWindow = function () { scrollBy(0, -offset)
-                              console.log("scrolled by " + offset);};
-window.addEventListener("hashchange", shiftWindow);
-function load() { if (window.location.hash) shiftWindow();}
 
 console.log(offset);
 $('body').scrollspy({
